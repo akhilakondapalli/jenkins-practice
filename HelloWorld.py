617553bc05e93985d7ae685cc6abdc3933ee0af7
akhila <akhila.kondapalli@iiitb.ac.in>
2022-02-10 09:00:26 +0000

    second change

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,4 @@
 #! /usr/bin/python3
-print("Modified Hello World - from remote rep...\n")
-print("Modified Hello World - from local rep...\n")
+print(" Hello World - from remote rep...\n")
+print(" Hello World - from local rep...\n")
 
